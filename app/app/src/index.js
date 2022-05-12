bb884c99906d54aae5648a1629630a58834a2d3b
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-05-12 16:13:29 +0200

    Add AdminRoot

--- app/app/src/index.js
+++ app/app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import EntryPage from './Login/EntryPage';
+import './index.css';
+
+import AdminRoot from './Admin/AdminRoot'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<EntryPage />);
+root.render(<AdminRoot />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
