725c7289a28fe95b62eaa5605988ce9fdb677cc1
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-05-19 13:41:34 +0200

    Modify index

--- app/app/src/index.js
+++ app/app/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import EntryPage from './Login/EntryPage';
+import CreateUser from './AdminCreateUser/CreateUser'
 import './index.css';
 
-import AdminRoot from './Admin/AdminRoot'
+import AdminRoot from './AdminRoot/AdminRoot'
+import UserRoot from './UserRoot/UserRoot';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AdminRoot />);
+root.render(<UserRoot userName={'Cippo'}/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
