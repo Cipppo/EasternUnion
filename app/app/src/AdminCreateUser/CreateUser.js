97e3dde29f43f64de3ab31e1c6b20123909a60d5
Luca Rapolla <luca.rapolla@studio.unibo.it>
2022-06-14 00:21:54 +0200

    switch to function

--- app/app/src/AdminCreateUser/CreateUser.js
+++ app/app/src/AdminCreateUser/CreateUser.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom"
 
 
-class CreateUser extends React.Component{
+const CreateUser = () => {
     
-    getDate = () => {
+    const getDate = () => {
         if(!this.state.startDate.getDate){
             console.log("Data vuota");
         }else{
@@ -14,55 +14,49 @@ class CreateUser extends React.Component{
         }
     }
 
-    onChange = (key, value) =>{
+    const onChange = (key, value) =>{
         this.setState({
             [key] : value
         })
     }
 
 
-    currentView = () => {
-        return(
-            <form>
-                <h2>Create new User</h2>
-                <fieldset>
-                    <ul>
-                        <li>
-                            <label htmlFor="Name">Name:</label>
-                            <input type="text" name="Name" id="Name" required/>
-                        </li>
-                        <li>
-                            <label htmlFor="Surname">Surname:</label>
-                            <input type="text" name="Surname" id="Surname" required/>
-                        </li>
-                        <li>
-                                <label htmlFor="DOB">Date of Birth:</label>
-                                <input type="date" onChange={(event) => this.setState({startDate: event.target.value})}/>
-                        </li>
-                        <li>
-                                <label htmlFor="Username">Username:</label>
-                                <input type="text" name="username" id="username" required/>
-                        </li>
-                        <li>
-                                <label htmlFor="Password">Password:</label>
-                                <input type="text" name="Password" id="Password" required/>
-                        </li>
-                    </ul>
-                </fieldset>
-                <button type="button" onClick={() => this.getDate()}>Submit</button>
 
-            </form>
-        )
-    }
+    return(
+        <section id="user-creation">
+        <form>
+            <h2>Create new User</h2>
+            <fieldset>
+                <ul>
+                    <li>
+                        <label htmlFor="Name">Name:</label>
+                        <input type="text" name="Name" id="Name" required/>
+                    </li>
+                    <li>
+                        <label htmlFor="Surname">Surname:</label>
+                        <input type="text" name="Surname" id="Surname" required/>
+                    </li>
+                    <li>
+                            <label htmlFor="DOB">Date of Birth:</label>
+                            <input type="date" onChange={(event) => this.setState({startDate: event.target.value})}/>
+                    </li>
+                    <li>
+                            <label htmlFor="Username">Username:</label>
+                            <input type="text" name="username" id="username" required/>
+                    </li>
+                    <li>
+                            <label htmlFor="Password">Password:</label>
+                            <input type="text" name="Password" id="Password" required/>
+                    </li>
+                </ul>
+            </fieldset>
+            <button type="button" onClick={() => this.getDate()}>Submit</button>
 
+        </form>
+        </section>
+    )
+    
 
-    render(){
-        return(
-            <section id="user-creation">
-                {this.currentView()}
-            </section>
-        )
-    }
 }
 
 export default CreateUser
